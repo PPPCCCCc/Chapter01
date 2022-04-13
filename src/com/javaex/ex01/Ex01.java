8656c7d85f003ba86cc7f2decf574101c2f324fc
ParkChanHee <ParkChanHee@210.123.107.251>
2022-04-13 16:09:49 +0900

    1

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,31 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+		public static void main(String[]args) { 
+			
+			/*int myAge; 변수 선언               int myAge=25; 변수+초기화
+			myAge =25; 초기화
+			System.out.println(myAge);
+			myAge=26;
+			
+	  	    */
+			byte no;
+			no=-128;
+			System.out.println(no);
+			
+			/*int 범위까지는 L을 붙이지 않아도된다.*/
+			/*int 범위를 벗어나는경우 L을 붙인다..*/
+			long no2;
+			no2=2147483648L;
+			System.out.println(no2);
+			
+			//변수 여러개일때 선언후 초기화
+			int var01;
+			int var02;
+			int var03;
+			
+			var01=10;
+			var02=20;
+			var03=30;
+}
+}
